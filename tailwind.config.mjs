e230b74277b770212ed84f249745774ccdbbb32c
facuramosg <facundoramosgarde@gmail.com>
2023-11-21 16:32:33 -0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				'fluid': 'repeat(auto-fit, minmax(120px, 1fr))',
+			  },
+		},
+	},
+	plugins: [],
+}
